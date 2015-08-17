2c51b3ff55e7603199d6f92abdfff873af962afc
Alexander Ortiz <aorti017@ucr.edu>
2015-08-17 15:18:23 -0700

    added more tests and finished tenative learning code

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ import os
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
-westRootDir = "./res/train/west"
-eastRootDir = "./res/train/east"
+westTrainDir = "./res/train/west"
+eastTrainDir = "./res/train/east"
+westTestDir = "./res/test/west"
+eastTestDir = "./res/test/east"
 
 lyrics = []
 coast = []
+testSet = []
 
-for subdir, dirs, files in os.walk(westRootDir):
+for subdir, dirs, files in os.walk(westTrainDir):
 	for f in files:
 		pth = os.path.join(subdir, f)
 		lyrics.append(open(pth).read())
 		coast.append('W')
-for subdir, dirs, files in os.walk(eastRootDir):
+for subdir, dirs, files in os.walk(eastTrainDir):
 	for f in files:
 		pth = os.path.join(subdir, f)
 		lyrics.append(open(pth).read())
@@ -27,3 +30,19 @@ tfidfTrain = tfidfTransformer.transform(trainCount)
 
 classifier = MultinomialNB().fit(tfidfTrain, coast)
 
+for subdir, dirs, files in os.walk(westTestDir):
+	for f in files:
+		print f
+		pth = os.path.join(subdir, f)
+		testSet.append(open(pth).read())
+for subdir, dirs, files in os.walk(eastTestDir):
+	for f in files:
+		print f
+		pth = os.path.join(subdir, f)
+		testSet.append(open(pth).read())
+
+testCount = countVec.transform(testSet)
+tfidfTest = tfidfTransformer.transform(testCount)
+
+predicted = classifier.predict(tfidfTest)
+print predicted
