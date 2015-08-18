78e142c3226bc8de648051b52384e08f09d78b5a
Alexander Ortiz <aorti017@ucr.edu>
2015-08-18 07:49:39 -0700

    punctuation now removed from lyrics and lyrics are stemmed before processing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
+import os, string, codecs
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
 
 westTrainDir = "./res/train/west"
 eastTrainDir = "./res/train/east"
@@ -11,21 +13,38 @@ lyrics = []
 coast = []
 testSet = []
 
+stemmer = SnowballStemmer("english")
+
 for subdir, dirs, files in os.walk(westTrainDir):
 	for f in files:
+		temp = ""
 		pth = os.path.join(subdir, f)
-		lyrics.append(open(pth).read())
+		fileLyrics = open(pth).read().decode('utf-8')
+		fileLyrics = fileLyrics.replace(string.punctuation, "")
+		for i in string.punctuation:
+			fileLyrics = fileLyrics.replace(i, '')
+		for i in fileLyrics.split(" "):
+			temp += stemmer.stem(i) + " "
+		lyrics.append(temp)
 		coast.append('W')
 for subdir, dirs, files in os.walk(eastTrainDir):
 	for f in files:
+		temp = ""
 		pth = os.path.join(subdir, f)
-		lyrics.append(open(pth).read())
+		fileLyrics = open(pth).read().decode('utf-8')
+		fileLyrics = fileLyrics.replace(string.punctuation, "")
+		for i in fileLyrics.split(" "):
+			temp += stemmer.stem(i) + " "
+		lyrics.append(temp)
 		coast.append('E')
+
+sw = stopwords.words("english")
+
 
 countVec = CountVectorizer()
 trainCount = countVec.fit_transform(lyrics)
 
-tfidfTransformer = TfidfTransformer(use_idf=False).fit(trainCount)
+tfidfTransformer = TfidfTransformer().fit(trainCount)
 tfidfTrain = tfidfTransformer.transform(trainCount)
 
 classifier = MultinomialNB().fit(tfidfTrain, coast)
